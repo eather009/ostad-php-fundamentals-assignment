4182dd638d82dfce69864b1a646f3158e4a1e726
OSAKA-IFTEKHAR <eather@export-japan.co.jp>
2023-09-12 01:25:30 +0900

    Weather report

--- assignment-1/weather_report.php
+++ assignment-1/weather_report.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Task 5:Weather Report</title>
+</head>
+<body>
+<form method="post" action="">
+    <label for="temperature">Temperature:</label>
+    <input type="number" id="temperature" name="temperature" required
+           value="<?php echo(!empty($_POST["temperature"]) ? $_POST['temperature'] : 0) ?>">
+    <input type="submit" value="Report">
+</form>
+
 <?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $temperature = $_POST["temperature"];
+
+    if($temperature <=10){
+        echo "It's freezing!";
+    }elseif($temperature <=20){
+        echo "It's cool!";
+    }else{
+        echo "It's warm!";
+    }
+}
+?>
+</body>
+</html>
