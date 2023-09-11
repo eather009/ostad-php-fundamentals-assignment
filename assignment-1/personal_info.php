f72c08f18580d4f4edc07261400d735e079a2371
Iftekhar Ahmed Eather <eather.ahmed@gmail.com>
2023-09-12 00:40:17 +0900

    Personal Information

---
+++ assignment-1/personal_info.php
@@ -0,0 +1,10 @@
+<?php
+$name = 'Iftekhar Ahmed Eather';
+$age = '32';
+$country = 'Bangladesh';
+$a_brief_introduction = 'I am living in Japan now for my Job. I am working as a System Engineer and Project Manager.';
+
+echo $name . PHP_EOL;
+echo $age . PHP_EOL;
+echo $country . PHP_EOL;
+echo $a_brief_introduction;
